abcd39465f96a63f4c06427fef437c757dc07303
Ronnie <rlrodrig88@gmail.com>
2020-07-22 14:51:38 -0500

    switched to cheerio

--- scrapeQuote.js
+++ scrapeQuote.js
@@ -1,81 +1,18 @@
-const puppeteer = require('puppeteer');
+const axios = require("axios")
+const cheerio = require("cheerio")
 
 module.exports = async function scrape(symbol) {
-  
-  const blockedResourceTypes = [
-    'image',
-    'media',
-    'font',
-    'texttrack',
-    'object',
-    'beacon',
-    'csp_report',
-    'imageset',
-  ];
-  
-  const skippedResources = [
-    'quantserve',
-    'adzerk',
-    'doubleclick',
-    'adition',
-    'exelator',
-    'sharethrough',
-    'cdn.api.twitter',
-    'google-analytics',
-    'googletagmanager',
-    'google',
-    'fontawesome',
-    'facebook',
-    'analytics',
-    'optimizely',
-    'clicktale',
-    'mixpanel',
-    'zedo',
-    'clicksor',
-    'tiqcdn',
-  ];
 
-  const url = 'https://finance.yahoo.com/quote/';
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  async function fetchHTML(url) {
+    const { data } = await axios.get(url)
+    return cheerio.load(data)
+  }
 
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.setRequestInterception(true);
-  
-  page.on('request', (req) => {
-      if (
-        blockedResourceTypes.indexOf(req.resourceType()) !== -1 ||
-        skippedResources.some(resource => url.indexOf(resource) !== -1)
-      ) {
-          req.abort();
-      } else { req.continue() }
-  });
+  const $ = await fetchHTML("https://finance.yahoo.com/quote/" + symbol)
 
-  const response = await page.goto(url + symbol, {
-    timeout: 25000,
-    waitUntil: 'networkidle2',
-  });
-
-
-
-  // Stock Price
-  const [el1] = await page.$x('//*[@id="quote-header-info"]/div[3]/div/div/span[1]');
-  const price = await el1.getProperty('textContent');
-  const rawPrice = await price.jsonValue();
-
-  // EPS
-  const [el2] = await page.$x('//*[@id="quote-summary"]/div[2]/table/tbody/tr[4]/td[2]/span');
-  const eps = await el2.getProperty('textContent');
-  const rawEps = await eps.jsonValue();
-  
-  // PE Ratio
-  const [el3] = await page.$x('//*[@id="quote-summary"]/div[2]/table/tbody/tr[3]/td[2]/span');
-  const pe = await el3.getProperty('textContent');
-  const rawPe = await pe.jsonValue();
-
-  console.log({rawPrice, rawEps, rawPe});
-
-  browser.close();
+  const rawPrice = $('#quote-header-info').find('[data-reactid~=14]').text()
+  const rawEps = $('#quote-summary').find('[data-reactid~=119]').text()
+  const rawPe = $('#quote-summary').find('[data-reactid~=124]').text()
 
   return {
     "Price": rawPrice,
