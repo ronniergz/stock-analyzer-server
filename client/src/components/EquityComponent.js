6a90d7e11739b76299757b0c84a8c2bdd10f5581
Ronnie <rlrodrig88@gmail.com>
2021-02-15 10:40:38 -0600

    removed api user functions

--- client/src/components/EquityComponent.js
+++ client/src/components/EquityComponent.js
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 import FormButton from './FormButtonComponent';
 import {device} from './device';
 import {Theme} from './theme';
-import path from 'path';
-import fs from 'fs';
-import https from 'https';
 
 const TableRow = styled.div`
   display: table-row;
@@ -134,47 +131,15 @@ class Equity extends Component {
     });
   }
 
-  getData() {
-
-    // const options = {
-    // //   // when using this code in production, for high throughput you should not read
-    // //   //   from the filesystem for every call, it can be quite expensive. Instead
-    // //   //   consider storing these in memory
-    //   cert: fs.readFileSync(
-    //     path.resolve(__dirname, '/etc/letsencrypt/live/stock-analyzer.xyz/fullchain.pem'),
-    //     `utf-8`,
-    //   ),
-    //   key: fs.readFileSync(
-    //     path.resolve(__dirname, '/etc/letsencrypt/live/stock-analyzer.xyz/privkey.pem'),
-    //     'utf-8',
-    //   ),
-    //   //passphrase:fd
-    //   //  '<your-passphrase>',
-  
-    //   // in test, if you're working with self-signed certificates
-    //   rejectUnauthorized: false,
-    //   // ^ if you intend to use this in production, please implement your own
-    //   //  `checkServerIdentity` function to check that the certificate is actually
-    //   //  issued by the host you're connecting to.
-    //   //
-    //   //  eg implementation here:
-    //   //  https://nodejs.org/api/https.html#https_https_request_url_options_callback
-  
-    //   keepAlive: false, // switch to true if you're making a lot of calls from this client
-    // };
-
-    // const sslConfiguredAgent = new https.Agent(options);
-
+  getData() {   
     // request current scraped data from server: 
-    fetch('https://stock-analyzer.xyz:3443/api/scrape?symbol=' + this.state.symbol, {
-      // agent: sslConfiguredAgent
+    fetch('http://localhost:5000/api/scrape?symbol=' + this.state.symbol, {
     })
       .then((response) => {
         if (response.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' + response.status);
           return;
         }
-        //response.text()
         response.json().then((data) => {
           this.setState({
             price: data.Price,
@@ -184,7 +149,6 @@ class Equity extends Component {
           this.getMos();
         });
       })
-      //.then(text => console.log(text))
       .catch(function (err) {
         console.log('Fetch Error :-S', err);
       });
--- server.js
+++ server.js
@@ -1,159 +1,21 @@
-//require('./api/data/db.js');
-//const mongoose = require('mongoose');
-//const Equity = require('./api/data/models/equity');
-//const User = require('./api/data/models/user');
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const scrape = require('./scrapeQuote.js');
 const fs = require('fs');
-const https = require('https');
+const http = require('http');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.set('secPort', port+443);
-
-const credentials = {
-  cert: fs.readFileSync('/etc/letsencrypt/live/stock-analyzer.xyz/fullchain.pem',`utf-8`),
-  key: fs.readFileSync('/etc/letsencrypt/live/stock-analyzer.xyz/privkey.pem', 'utf-8')
-};
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(app.get('secPort'), () => {
-  console.log("Server is listening on port", app.get('secPort'));
-})
+app.listen(port, () => {
+  console.log('Server is listening on port ', port);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/*///////////     Future REST API      //////////////////*/
-
-/*-------------    Users   -----------------*/
-
-// Return a list of all users
-app.get('/api/users', (req, res) => {
-  User.find().exec(function (err, users) {
-    console.log('Found users', users.length);
-    res.json(users);
-  });
-});
-
-// Add a new user
-app.post('/api/users', (req, res) => {
-  let newUser = {
-    username: req.body.username,
-    password: req.body.password,
-  };
-  User.create(newUser, function (err, user) {
-    if (err) {
-      console.log(`Error adding new user`, err);
-      res.status(400).json(err);
-    } else {
-      console.log(`Added new user`, user);
-      res.status(201).json(user);
-    }
-  });
-});
-
-// Get a specific user
-app.get('/api/users/:userId', (req, res) => {
-  let id = req.params.userId;
-  User.findById(id).exec(function (err, user) {
-    res.json(user);
-  });
-});
-
-// Update specific user
-app.put('/api/users/:userId', (req, res) => {
-  let updatedUser = {};
-  if (req.body.username) updatedUser.username = req.body.username;
-  if (req.body.password) updatedUser.password = req.body.password;
-  User.findByIdAndUpdate(req.params.userId, updatedUser, {new: true}, function (err, user) {
-    if (err) {
-      console.log(`Error updating new user`, err);
-      res.status(400).json(err);
-    } else {
-      console.log(`updated new user`, user);
-      res.status(201).json(user);
-    }
-  });
-});
-
-// Delete a specific user
-app.delete('/api/users/:userId', (req, res) => {
-  let id = req.params.userId;
-  User.findByIdAndRemove(id).exec(function (err, user) {
-    if (err) {
-      res.status(404).json(err);
-    } else {
-      console.log('User deleted, id:', id);
-      res.status(204).json(user);
-    }
-  });
-});
-
-// Return a specific user's watchlist
-app.get('/api/users/:userId/watchlist', (req, res) => {
-  User.findById(req.params.userId).exec(function (err, user) {
-    res.json(user.wishlist);
-  });
-});
-
-// Add a new equity to user's watchlist
-app.post('/api/users/:userId/watchlist', (req, res) => {
-  let newEquity = {
-    symbol: req.body.symbol,
-    growth: req.body.growth,
-    futpe: req.body.futpe,
-  };
-  User.findByIdAndUpdate(req.params.userId, {$push: {wishlist: newEquity}}, {new: true}).exec(function (err, user) {
-    console.log(`Added new equity to user's watchlist`, user.wishlist);
-    res.json(user);
-  });
-});
-
-// Get specific item on user's watchlist
-app.get('/api/users/:userId/watchlist/:equityId', (req, res) => {
-  User.findById(req.params.userId)
-    .select('wishlist')
-    .exec(function (err, user) {
-      let equity = user.wishlist.id(req.params.equityId);
-      res.status(200).json(equity);
-    });
-});
-
-// Update item on user's watchlist
-app.put('/api/users/:userId/watchlist/:equityId', (req, res) => {
-  User.findById(req.params.userId)
-    .select('wishlist')
-    .exec(function (err, user) {
-      let equity = user.wishlist.id(req.params.equityId);
-      if (req.body.growth) equity.growth = req.body.growth;
-      if (req.body.futpe) equity.futpe = req.body.futpe;
-      if (err) {
-        console.log(`Error updating new user`, err);
-        res.status(400).json(err);
-      } else {
-        console.log(`updated new user`, user);
-        res.status(201).json(user);
-      }
-    });
-});
-
-// Delete item on user's watchlist
-app.delete('/api/users/:userId/watchlist/:symbol', (req, res) => {
-  User.findByIdAndUpdate(req.params.userId, {$pull: {wishlist: {symbol: req.params.symbol}}}, {new: true}).exec(function (err, user) {
-    console.log(`Removed equity from user's watchlist`, user.wishlist);
-    res.json(user);
-  });
-});
-
-/*///////////                        //////////////////*/
-
 /*---------------  Web Scraper  ---------------*/
-
 // Get current equity data
 app.get('/api/scrape', cors(), (req, res) => {
   scrape(req.query.symbol).then((value) => {
@@ -165,8 +27,6 @@ app.get('/api/scrape', cors(), (req, res) => {
 app.get('/api/test', cors(), (req, res) => {
   res.send("Hello!");
 });
-
-//const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 process.once('SIGUSR2', function () {
   httpsServer.close(function () {
